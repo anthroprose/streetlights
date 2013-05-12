9a49bce85222a7521135ddf583484cabf83dd6f5
Alex Corley <anthroprose@gmail.com>
2013-05-11 21:07:57 -0500

    WORKING color wheel!

--- demo/rfRangeRXBlink.ino
+++ demo/rfRangeRXBlink.ino
@@ -1,65 +1,28 @@
-/// @dir rfRangeRX
-/// Receive sketch used to report quality of reception.
-// 2011-05-14 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
-
-// Based on a sample implementation by Steve Evans (@tankslappa).
-
 #include <JeeLib.h>
 #include "HughesyShiftBrite.h"
 
-#define NODEID 1
-#define BUFFER_SIZE 64
-#define DISPLAY_INTERVAL 500 // ms
-
-MilliTimer displayTimer;
-
-byte timeBuf [BUFFER_SIZE]; // index is time slot, value is last packet
-byte seqBuf [BUFFER_SIZE];  // index is last packet, value is time slot
-char history [11];
-byte lastSeq;
 HughesyShiftBrite sb;
 
-static void gotPacket () {
-  byte tenths = millis() / 100;
-  // remember for each time slot what the last received packet was
-  timeBuf[tenths % BUFFER_SIZE] = lastSeq;
-  // remember for the last BUFFER_SIZE packets when they arrived
-  seqBuf[lastSeq % BUFFER_SIZE] = tenths;
-}
-
-static byte recvCount (byte period) {
-  // tenths and diff are bytes, so they are automatically modulo 256
-  byte tenths = millis() / 100;
-  byte n = 0;
-  for (byte i = 0; i < sizeof seqBuf; ++i) {
-    byte diff = tenths - seqBuf[i];
-    if (diff <= period)
-      ++n;
-  }
-  return n;
-}
-
-static void updateHistory () {
-  for (byte i = 1; i < 10; ++i)
-    history[10-i] = history[9-i];
-  history[0] = '0';
-  // tenths and diff are bytes, so they are automatically modulo 256
-  byte tenths = millis() / 100;
-  for (byte i = 0; i < DISPLAY_INTERVAL / 100; ++i) {
-    byte pos = (tenths - i - 1) % BUFFER_SIZE;
-    byte diff = lastSeq - timeBuf[pos];
-    if (diff < 5)
-      ++history[0];
-  }
-}
-
 void setup () {
+  
   Serial.begin(57600);
-  rf12_initialize(NODEID, RF12_868MHZ, 88);
-  // synchronize the display to 0.1s clock transitions
-  displayTimer.set(DISPLAY_INTERVAL - millis() % DISPLAY_INTERVAL - 1);
+  rf12_initialize(4, RF12_915MHZ, 44);
+  
   Serial.println("Setup");
-  sb = HughesyShiftBrite(8,11,12,13);
+  
+  //(int dataPin, int latchPin, int enablePin, int clockPin)
+  sb = HughesyShiftBrite(7,6,5,4);
+  
+  sb.sendColour(255,0,0);
+  sb.sendColour(255,0,0);
+  delay(1000);
+  sb.sendColour(0,255,0);
+  sb.sendColour(0,255,0);
+  delay(1000);
+  sb.sendColour(0,0,255);
+  sb.sendColour(0,0,255);
+  delay(1000);
+  
 }
 
 void loop () {
@@ -68,14 +31,23 @@ void loop () {
     
     sb.sendColour(rf12_data[0],rf12_data[1],rf12_data[2]);
     sb.sendColour(rf12_data[0],rf12_data[1],rf12_data[2]);
+
+    Serial.print((int) rf12_hdr);
+    Serial.print(':'); 
+    Serial.print((int) rf12_grp);
+    Serial.print(':'); 
+    Serial.print((int) RF12_HDR_DST);
+    Serial.print(':'); 
+    Serial.print((int) RF12_HDR_ACK);
     
-    Serial.print("Colors: ");
+    Serial.print(" - Colors: ");
     Serial.print(rf12_data[0]);
     Serial.print(",");
     Serial.print(rf12_data[1]);
     Serial.print(",");
-    Serial.println(rf12_data[2]);
+    Serial.print(rf12_data[2]);
+    Serial.println("");
     
   }
-  
+
 }
--- demo/rfRangeTXBlink.ino
+++ demo/rfRangeTXBlink.ino
@@ -1,89 +1,172 @@
-/// @dir rfRangeTX
-/// Transmit sketch used to report quality of reception.
-// 2011-05-14 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
-
-// Based on a sample implementation by Steve Evans (@tankslappa).
-
 #include <JeeLib.h>
-
-byte outData[3], pending;
-MilliTimer sendTimer;
+#define numNodes 5
+byte outData[numNodes][3];
 
 void setup() {
   Serial.begin(57600);
-  rf12_initialize(13, RF12_868MHZ, 88);
+  rf12_initialize(26, RF12_915MHZ, 44);
   Serial.println("Setup");
+}
+
+void sendSig() {
+
+  rf12_recvDone();
+      
+  for (int i = 1; i <= numNodes; i++) {
+  
+    if (rf12_canSend()) {
+      
+      byte header = 0;
+      
+      if (i)
+            header |= RF12_HDR_DST | i;
+            
+      rf12_sendStart(header, &outData[i-1], sizeof outData[i-1], 1);
+      delay(50);
+      Serial.print("Sending node:");  
+      Serial.print(i);  
+      Serial.print(" rgb:");  
+      Serial.print(outData[i-1][0]);  
+      Serial.print(",");  
+      Serial.print(outData[i-1][1]);
+      Serial.print(",");  
+      Serial.print(outData[i-1][2]);
+      Serial.println("");  
+      rf12_recvDone();
+
+    }
+      
+  }
+
+  rf12_recvDone();
+  
 }
 
 void loop() {
   
-  outData[0] = 0;
-  outData[1] = 0;
-  outData[2] = 0;
+  outData[0][0] = 200;
+  outData[0][1] = 0;
+  outData[0][2] = 0;
   
-  rf12_recvDone();
-    
-  if (sendTimer.poll(100)) {
-    ++outData[0];
-    pending = 1;
-  }
+  outData[1][0] = 0;
+  outData[1][1] = 200;
+  outData[1][2] = 0;
+  
+  outData[2][0] = 0;
+  outData[2][1] = 0;
+  outData[2][2] = 200;
+  
+  outData[3][0] = 200;
+  outData[3][1] = 0;
+  outData[3][2] = 0;
 
-  for (int i = 1; i<4;i++) {
-  
-    if (pending && rf12_canSend()) {
-      rf12_sendStart(i, &outData, sizeof outData);
-      Serial.println("Sending");
-      delay(1000);
-    }
-  
-    pending = 0;
-        
-  }
-    
-  outData[0] = 0;
-  outData[1] = 0;
-  outData[2] = 0;
-  
-  rf12_recvDone();
-    
-  if (sendTimer.poll(100)) {
-    ++outData[1];
-    pending = 1;
-  }
+  outData[4][0] = 0;
+  outData[4][1] = 200;
+  outData[4][2] = 0;
 
-  for (int i = 1; i<4;i++) {
+  sendSig();
+  delay(1000);
   
-    if (pending && rf12_canSend()) {
-      rf12_sendStart(i, &outData, sizeof outData);
-      Serial.println("Sending");
-      delay(1000);
+  outData[0][0] = 0;
+  outData[0][1] = 200;
+  outData[0][2] = 0;
+  
+  outData[1][0] = 0;
+  outData[1][1] = 0;
+  outData[1][2] = 200;
+  
+  outData[2][0] = 200;
+  outData[2][1] = 0;
+  outData[2][2] = 0;
+
+  outData[3][0] = 0;
+  outData[3][1] = 200;
+  outData[3][2] = 0;
+
+  outData[4][0] = 200;
+  outData[4][1] = 0;
+  outData[4][2] = 0;
+
+  sendSig();
+  delay(1000);
+  
+  outData[0][0] = 0;
+  outData[0][1] = 0;
+  outData[0][2] = 200;
+  
+  outData[1][0] = 200;
+  outData[1][1] = 0;
+  outData[1][2] = 0;
+
+  outData[2][0] = 0;
+  outData[2][1] = 200;
+  outData[2][2] = 0;
+
+  outData[3][0] = 200;
+  outData[3][1] = 0;
+  outData[3][2] = 0;
+
+  outData[4][0] = 0;
+  outData[4][1] = 200;
+  outData[4][2] = 0;
+
+  sendSig();
+  delay(1000);
+  
+}
+
+void rainbowCycle() {
+  
+  int i, j;
+  
+  for (j=0; j < 256; j++) {
+  
+    for (i=1; i <= numNodes; i++) {
       
-    }
-  
-    pending = 0;
-        
-  }
-  
-  outData[0] = 0;
-  outData[1] = 0;
-  outData[2] = 0;
-  
-  rf12_recvDone();
+      setPixelColor(i, Wheel( ((i * 256 / numNodes) + j) % 256) );
+       
+    }  
     
-  if (sendTimer.poll(100)) {
-    ++outData[2];
-    pending = 1;
+    sendSig(); 
+    delay(1000);
+    
   }
+  
+}
 
-  for (int i = 1; i<4;i++) {
+// Set pixel color from 'packed' 32-bit RGB value:
+void setPixelColor(uint16_t n, uint32_t c) {
   
-    if (pending && rf12_canSend()) {
-      rf12_sendStart(i, &outData, sizeof outData);
-      Serial.println("Sending");  
-      delay(1000);
-    }
-  
-    pending = 0;
-        
+  if (n < numNodes) { // Arrays are 0-indexed, thus NOT '<='
+      outData[n][0] = c >>  8; // Green
+      outData[n][1] = c >> 16; // Red
+      outData[n][2] = c;       // Blue
+  }
+}
+
+// Create a 24 bit color value from R,G,B
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
+
+//Input a value 0 to 255 to get a color value.
+//The colours are a transition r - g -b - back to r
+uint32_t Wheel(byte WheelPos)
+{
+  if (WheelPos < 85) {
+   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+   WheelPos -= 85;
+   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170; 
+   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
